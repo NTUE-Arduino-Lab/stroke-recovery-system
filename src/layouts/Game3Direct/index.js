4c0364bd7472a682f06119291ddb6b7a6f299639
Murphy Hsieh <murphyhsieh@leandev.io>
2022-10-30 16:26:44 +0800

    feat: fix the game direct image

--- src/layouts/Game3Direct/index.js
+++ src/layouts/Game3Direct/index.js
@@ -417,7 +417,7 @@ const Game3Direct = () => {
                         background: `url(${Dir_Img})`,
                         width: 500,
                         height: '54%',
-                        bottom: 100,
+                        bottom: 200,
                         right: 12,
                         position: 'absolute',
                         backgroundSize: '100%',
